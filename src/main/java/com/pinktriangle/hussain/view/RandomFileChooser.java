1ce058ba4d819a59e60eb338d51b4323e90c098e
Hussain Miah <hmiah.1993@gmail.com>
2013-10-27 13:14:55 +0000

    Basic GUI is there

--- src/main/java/com/pinktriangle/hussain/view/RandomFileChooser.java
+++ src/main/java/com/pinktriangle/hussain/view/RandomFileChooser.java
@@ -1,8 +1,11 @@
 package com.pinktriangle.hussain.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,10 +24,12 @@ public class RandomFileChooser extends JFrame {
 	private JButton chooseRandomFile;
 	private JButton cancel;
 	
+	private JFileChooser fileChooser;
+	
 	public RandomFileChooser(){
 		super("Random File Chooser");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(new Dimension(500, 150));
+		setSize(new Dimension(500, 110));
 		initWidgets();
 		setLayout();
 		addWidgets();
@@ -32,15 +37,34 @@ public class RandomFileChooser extends JFrame {
 	}
 	
 	private void initWidgets(){
+		panelCenter = new JPanel();
+		panelSouth = new JPanel();
 		
+		label = new JLabel("Chosen Folder:");
+		folderPath = new JTextField(20);
+		folderPath.setEnabled(false);
+		chooseFolder = new JButton("Choose Folder");
+		
+		chooseRandomFile = new JButton("Random File");
+		cancel = new JButton("Cancel");
 	}
 	
 	private void setLayout(){
-		
+		panelCenter.setLayout(new FlowLayout());
+		panelSouth.setLayout(new FlowLayout(FlowLayout.RIGHT));
 	}
 	
 	private void addWidgets(){
+		panelCenter.add(label);
+		panelCenter.add(folderPath);
+		panelCenter.add(chooseFolder);
 		
+		add(panelCenter,BorderLayout.CENTER);
+		
+		panelSouth.add(chooseRandomFile);
+		panelSouth.add(cancel);
+		
+		add(panelSouth, BorderLayout.SOUTH);
 	}
 	
 	private void addActionListeners(){
